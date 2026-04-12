e1d46ac6fa02bc2141e8c148769e91d721600df5
Shubhra Mishra <shubhra-email@example.com>
2026-04-12 14:44:12 +0530

    hi

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, CI/CD World!";
+    @GetMapping("/")
+    public String sayHello() {
+        return "hii";
     }
 }
